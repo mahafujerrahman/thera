d1e52c56c4b1aeb069cd7281609cef5234e5554e
mahafujer <mahafujer1999@gmail.com>
2025-03-11 09:51:04 +0600

    update

--- lib/helpers/di.dart
+++ lib/helpers/di.dart
@@ -17,6 +17,7 @@ Future<Map<String, Map<String, String>>> init() async {
   Get.lazyPut(() => LocalizationController(sharedPreferences: Get.find()));
 
 
+
   //Retrieving localized data
   Map<String, Map<String, String>> _languages = Map();
 
